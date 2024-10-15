ff4e426314c7155581f3723a61d7443a79e73b9a
Camila Fimbres <camifimbres@gmail.com>
2024-10-15 08:01:55 -0700

    Agregar archivos de proyecto.

---
+++ DiaSemana/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DiaSemana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dame un número :D");
+            string dato = Console.ReadLine();
+            int dia = 0;
+            bool sePudoconvertir = false;
+            sePudoconvertir = int.TryParse(dato, out dia); 
+
+            if (sePudoconvertir && dia >=1 && dia <=7)
+            {
+                Console.WriteLine(sePudoconvertir + " " + dia);
+            }
+        }
+    }
+}
